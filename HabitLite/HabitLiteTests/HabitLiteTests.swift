fb0cf0a982f9ca5d97592be840d8141aa64b509e
Yen-Kai Huang <86472793+Kai-0530@users.noreply.github.com>
2025-09-22 18:36:40 +0800

    Sketch

---
+++ HabitLite/HabitLiteTests/HabitLiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitLiteTests.swift
+//  HabitLiteTests
+//
+//  Created by 黃彥愷 on 2025/9/22.
+//
+
+import Testing
+@testable import HabitLite
+
+struct HabitLiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
